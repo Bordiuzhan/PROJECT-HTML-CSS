50343cd64fadd86edbe500c8d406ee2e576bfccb
MykolaDanyliuk <mykoladanyliuk.ua@gmail.com>
2022-07-24 12:53:15 +0300

    feature/header-id
    adds id-anchor, header-fixed, is-open in mob-mebu, menu.js, update styles

--- src/js/menu.js
+++ src/js/menu.js
@@ -10,7 +10,7 @@
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
+    refs.menu.classList.toggle('is-open');
     refs.body.classList.toggle('no-scroll');
   }
 })();
