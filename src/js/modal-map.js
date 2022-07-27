9b487ffc13dfea758b05d0f71528265d013f66b8
Irina Khorunz <irahorunz87@gmail.com>
2022-07-27 19:02:16 +0300

    Добавляет запрет прокрутки оверлея

--- src/js/modal-map.js
+++ src/js/modal-map.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector('[data-contacts__modal-open]'),
     closeModalBtn: document.querySelector('[data-contacts__modal-close]'),
     modal: document.querySelector('[data-contacts__modal-map]'),
+    body: document.querySelector('body'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -10,5 +11,6 @@
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
   }
 })();
